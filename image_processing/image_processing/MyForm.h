18f03f4c750a7c2ef5366745ba871b54a2ab0ea0
joey3639570 <joey3639570@gmail.com>
2021-10-14 16:42:33 +0800

    Add Mean and Median filter, not test yet. Histo Eq, Threshold not yet

--- image_processing/image_processing/MyForm.h
+++ image_processing/image_processing/MyForm.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 
 namespace imageprocessing {
 
@@ -43,6 +44,10 @@ namespace imageprocessing {
 	private: System::Windows::Forms::Button^ buttonGreen;
 	private: System::Windows::Forms::Button^ buttonBlue;
 	private: System::Windows::Forms::Button^ buttonGray;
+	private: System::Windows::Forms::Button^ buttonMean;
+	private: System::Windows::Forms::Button^ buttonMedian;
+	private: System::Windows::Forms::Button^ buttonHistogram;
+	private: System::Windows::Forms::Button^ buttonThreshold;
 	private:
 		/// <summary>
 		/// 設計工具所需的變數。
@@ -64,6 +69,8 @@ namespace imageprocessing {
 			this->buttonBlue = (gcnew System::Windows::Forms::Button());
 			this->buttonGreen = (gcnew System::Windows::Forms::Button());
 			this->buttonGray = (gcnew System::Windows::Forms::Button());
+			this->buttonMean = (gcnew System::Windows::Forms::Button());
+			this->buttonMedian = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picture_box1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picture_box2))->BeginInit();
 			this->SuspendLayout();
@@ -146,6 +153,46 @@ namespace imageprocessing {
 			this->buttonGray->UseVisualStyleBackColor = true;
 			this->buttonGray->Click += gcnew System::EventHandler(this, &MyForm::buttonGray_Click);
 			// 
+			// Mean Filter Button
+			// 
+			this->buttonGray->Location = System::Drawing::Point(288, 576);
+			this->buttonGray->Name = L"buttonMean";
+			this->buttonGray->Size = System::Drawing::Size(96, 32);
+			this->buttonGray->TabIndex = 8;
+			this->buttonGray->Text = L"Mean Filter";
+			this->buttonGray->UseVisualStyleBackColor = true;
+			this->buttonGray->Click += gcnew System::EventHandler(this, &MyForm::buttonMean_Click);
+			// 
+			// Median Filter Button
+			// 
+			this->buttonGray->Location = System::Drawing::Point(288, 640);
+			this->buttonGray->Name = L"buttonMedian";
+			this->buttonGray->Size = System::Drawing::Size(96, 32);
+			this->buttonGray->TabIndex = 9;
+			this->buttonGray->Text = L"Median Filter";
+			this->buttonGray->UseVisualStyleBackColor = true;
+			this->buttonGray->Click += gcnew System::EventHandler(this, &MyForm::buttonMedian_Click);
+			// 
+			// Histogram Equalization Button
+			// 
+			this->buttonGray->Location = System::Drawing::Point(416, 576);
+			this->buttonGray->Name = L"buttonHistogram";
+			this->buttonGray->Size = System::Drawing::Size(96, 32);
+			this->buttonGray->TabIndex = 10;
+			this->buttonGray->Text = L"Histogram Equalization";
+			this->buttonGray->UseVisualStyleBackColor = true;
+			this->buttonGray->Click += gcnew System::EventHandler(this, &MyForm::buttonHistogram_Click);
+			// 
+			// Threshold Button
+			// 
+			this->buttonGray->Location = System::Drawing::Point(416, 640);
+			this->buttonGray->Name = L"buttonThreshold";
+			this->buttonGray->Size = System::Drawing::Size(96, 32);
+			this->buttonGray->TabIndex = 11;
+			this->buttonGray->Text = L"Threshold";
+			this->buttonGray->UseVisualStyleBackColor = true;
+			this->buttonGray->Click += gcnew System::EventHandler(this, &MyForm::buttonThreshold_Click);
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
@@ -159,6 +206,10 @@ namespace imageprocessing {
 			this->Controls->Add(this->buttonGreen);
 			this->Controls->Add(this->buttonBlue);
 			this->Controls->Add(this->buttonGray);
+			this->Controls->Add(this->buttonMean);
+			this->Controls->Add(this->buttonMedian);
+			this->Controls->Add(this->buttonHistogram);
+			this->Controls->Add(this->buttonThreshold);
 			this->Name = L"MyForm";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picture_box1))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picture_box2))->EndInit();
@@ -176,6 +227,7 @@ namespace imageprocessing {
 
 		// int pointer for calculation
 		IntPtr ptr, GrayResultPtr, RedResultPtr, GreenResultPtr, BlueResultPtr;
+		IntPtr meanResultPtr, medianResultPtr;
 
 		// Byte pointer
 		Byte* p;
@@ -183,6 +235,8 @@ namespace imageprocessing {
 		Byte* G;
 		Byte* R;
 		Byte* B;
+		Byte* mean;
+		Byte* median;
 
 #pragma endregion
 	private: System::Void button1_click_1(System::Object^ sender, System::EventArgs^ e) {
@@ -505,6 +559,284 @@ namespace imageprocessing {
 		// show it on the pictureBox
 		picture_box2->Image = grayImage;
 	}
-	
+	private: System::Void buttonMean_Click(System::Object^ sender, System::EventArgs^ e) {
+		// Mean Filter
+
+		// declaration of BitMap object for results
+		Bitmap^ meanImage;
+		meanImage = gcnew Bitmap(image1->Width, image1->Height);
+
+		// declaration of BitMap object for pixels of results
+		Imaging::BitmapData^ meanImageData;
+
+		// Lock the image
+		meanImageData = meanImage->LockBits(rect, System::Drawing::Imaging::ImageLockMode::ReadWrite, image1->PixelFormat);
+
+		// Gray, set int ptr to the front of the image
+		meanResultPtr = meanImageData->Scan0;
+		mean = (Byte*)((Void*)GrayResultPtr);
+
+		// Lock the original image
+		ImageData1 = image1->LockBits(rect, System::Drawing::Imaging::ImageLockMode::ReadWrite, image1->PixelFormat);
+		// set int ptr to the front of the image
+		ptr = ImageData1->Scan0;
+		// ptr initialization
+		p = (Byte*)((Void*)ptr);
+
+		// process all pixel(without_boundary)
+		for (int y = 0; y < image1->Height; y++)
+		{
+			for (int x = 0; x < image1->Width; x++)
+			{
+				// boundary cases
+				if(y == 0) {
+					int b_idx = 3*x;
+					int g_idx = 3*x + 1;
+					int r_idx = 3*x + 2;
+					mean[b_idx] = p[b_idx];
+					mean[g_idx] = p[g_idx];
+					mean[r_idx] = p[r_idx];
+					continue;
+				}
+				else if(y == image1->Height-1 || x == 0 || x == image1->Width-1) {
+					int b_idx = 3*x + 3 * image1->Width * (y-1);
+					int g_idx = 3*x + 3 * image1->Width * (y-1) + 1;
+					int r_idx = 3*x + 3 * image1->Width * (y-1) + 2;
+					mean[b_idx] = p[b_idx];
+					mean[g_idx] = p[g_idx];
+					mean[r_idx] = p[r_idx];
+					continue;
+				}
+				
+				// index setup
+				int top_left_idx 	= 3*(x-1) + 3 * image1->Width * (y-1);
+				int top_idx 		= 3*(x)   + 3 * image1->Width * (y-1);
+				int top_right_idx 	= 3*(x+1) + 3 * image1->Width * (y-1);
+				int left_idx 		= 3*(x-1) + 3 * image1->Width * (y);
+				int mid_idx 		= 3*(x)   + 3 * image1->Width * (y);
+				int right_idx 		= 3*(x+1) + 3 * image1->Width * (y);
+				int bot_left_idx 	= 3*(x-1) + 3 * image1->Width * (y+1);
+				int bot_idx 		= 3*(x)   + 3 * image1->Width * (y+1);
+				int bot_right_idx 	= 3*(x+1) + 3 * image1->Width * (y+1);
+				
+				// mean_filter
+				int b_mean = (p[top_left_idx] + p[top_idx] + p[top_right_idx] 
+							+ p[left_idx] + p[mid_idx] + p[right_idx] 
+							+ p[bot_left_idx] + p[bot_idx] + p[bot_right_idx]) / 9
+				int g_mean = (p[top_left_idx+1] + p[top_idx+1] + p[top_right_idx+1] 
+							+ p[left_idx+1] + p[mid_idx+1] + p[right_idx+1] 
+							+ p[bot_left_idx+1] + p[bot_idx+1] + p[bot_right_idx+1]) / 9
+				int r_mean = (p[top_left_idx+2] + p[top_idx+2] + p[top_right_idx+2] 
+							+ p[left_idx+2] + p[mid_idx+2] + p[right_idx+2] 
+							+ p[bot_left_idx+2] + p[bot_idx+2] + p[bot_right_idx+2]) / 9
+				
+				// put averaged value into the result
+				mean[mid_idx] = (Byte)b_mean;
+				mean[mid_idx+1] = (Byte)g_mean;
+				mean[mid_idx+2] = (Byte)r_mean;
+
+			}
+		}
+
+		// Unlock pixel
+		image1->UnlockBits(ImageData1);
+		meanImage->UnlockBits(meanImageData);
+
+		// show it on the pictureBox
+		picture_box2->Image = meanImage;
+	}
+	private: System::Void buttonMedian_Click(System::Object^ sender, System::EventArgs^ e) {
+		// Median Filter
+
+		// declaration of BitMap object for results
+		Bitmap^ medianImage;
+		medianImage = gcnew Bitmap(image1->Width, image1->Height);
+
+		// declaration of BitMap object for pixels of results
+		Imaging::BitmapData^ medianImageData;
+
+		// Lock the image
+		medianImageData = medianImage->LockBits(rect, System::Drawing::Imaging::ImageLockMode::ReadWrite, image1->PixelFormat);
+
+		// Gray, set int ptr to the front of the image
+		medianResultPtr = medianImageData->Scan0;
+		median = (Byte*)((Void*)GrayResultPtr);
+
+		// Lock the original image
+		ImageData1 = image1->LockBits(rect, System::Drawing::Imaging::ImageLockMode::ReadWrite, image1->PixelFormat);
+		// set int ptr to the front of the image
+		ptr = ImageData1->Scan0;
+		// ptr initialization
+		p = (Byte*)((Void*)ptr);
+
+		// process all pixel(without_boundary)
+		for (int y = 0; y < image1->Height; y++)
+		{
+			for (int x = 0; x < image1->Width; x++)
+			{
+				// boundary cases
+				if(y == 0) {
+					int b_idx = 3*x;
+					int g_idx = 3*x + 1;
+					int r_idx = 3*x + 2;
+					median[b_idx] = p[b_idx];
+					median[g_idx] = p[g_idx];
+					median[r_idx] = p[r_idx];
+					continue;
+				}
+				else if(y == image1->Height-1 || x == 0 || x == image1->Width-1) {
+					int b_idx = 3*x + 3 * image1->Width * (y-1);
+					int g_idx = 3*x + 3 * image1->Width * (y-1) + 1;
+					int r_idx = 3*x + 3 * image1->Width * (y-1) + 2;
+					median[b_idx] = p[b_idx];
+					median[g_idx] = p[g_idx];
+					median[r_idx] = p[r_idx];
+					continue;
+				}
+				
+				// index setup
+				int top_left_idx 	= 3*(x-1) + 3 * image1->Width * (y-1);
+				int top_idx 		= 3*(x)   + 3 * image1->Width * (y-1);
+				int top_right_idx 	= 3*(x+1) + 3 * image1->Width * (y-1);
+				int left_idx 		= 3*(x-1) + 3 * image1->Width * (y);
+				int mid_idx 		= 3*(x)   + 3 * image1->Width * (y);
+				int right_idx 		= 3*(x+1) + 3 * image1->Width * (y);
+				int bot_left_idx 	= 3*(x-1) + 3 * image1->Width * (y+1);
+				int bot_idx 		= 3*(x)   + 3 * image1->Width * (y+1);
+				int bot_right_idx 	= 3*(x+1) + 3 * image1->Width * (y+1);
+				
+				// array setup
+				int b_sorted_array[9] = {p[top_left_idx], p[top_idx], p[top_right_idx],
+										p[left_idx], p[mid_idx], p[right_idx], 
+										p[bot_left_idx], p[bot_idx], p[bot_right_idx]}
+				int g_sorted_array[9] = {p[top_left_idx+1], p[top_idx+1], p[top_right_idx+1],
+										p[left_idx+1], p[mid_idx+1], p[right_idx+1], 
+										p[bot_left_idx+1], p[bot_idx+1], p[bot_right_idx+1]}
+				int r_sorted_array[9] = {p[top_left_idx+2], p[top_idx+2], p[top_right_idx+2],
+										p[left_idx+2], p[mid_idx+2], p[right_idx+2], 
+										p[bot_left_idx+2], p[bot_idx+2], p[bot_right_idx+2]}
+				
+				// sort array
+				std::sort(b_sorted_array, b_sorted_array+9);
+				std::sort(g_sorted_array, g_sorted_array+9);
+				std::sort(r_sorted_array, r_sorted_array+9);
+				
+				// median_filter
+				// put median into the result
+				median[mid_idx] = (Byte)b_sorted_array[5];
+				median[mid_idx+1] = (Byte)g_sorted_array[5];
+				median[mid_idx+2] = (Byte)r_sorted_array[5];
+
+			}
+		}
+
+		// Unlock pixel
+		image1->UnlockBits(ImageData1);
+		medianImage->UnlockBits(medianImageData);
+
+		// show it on the pictureBox
+		picture_box2->Image = medianImage;
+	}
+	private: System::Void buttonHistogram_Click(System::Object^ sender, System::EventArgs^ e) {
+		// Histogram Equalization
+
+		// declaration of BitMap object for results
+		Bitmap^ histImage;
+		histImage = gcnew Bitmap(image1->Width, image1->Height);
+
+		// declaration of BitMap object for pixels of results
+		Imaging::BitmapData^ histImageData;
+
+		// Lock the image
+		histImageData = histImage->LockBits(rect, System::Drawing::Imaging::ImageLockMode::ReadWrite, image1->PixelFormat);
+
+		// hist, set int ptr to the front of the image
+		histResultPtr = histImageData->Scan0;
+		Gr = (Byte*)((Void*)histResultPtr);
+
+		// Lock the original image
+		ImageData1 = image1->LockBits(rect, System::Drawing::Imaging::ImageLockMode::ReadWrite, image1->PixelFormat);
+		// set int ptr to the front of the image
+		ptr = ImageData1->Scan0;
+		// ptr initialization
+		p = (Byte*)((Void*)ptr);
+
+		// process all pixel
+		for (int y = 0; y < image1->Height; y++)
+		{
+			for (int x = 0; x < image1->Width; x++)
+			{
+				int pixel = (p[0] * 11 + p[1] * 59 + p[2] * 30 + 50) / 100;
+				Gr[0] = (Byte)pixel;
+				Gr[1] = (Byte)pixel;
+				Gr[2] = (Byte)pixel;
+
+				// Move to next pixel
+				Gr += 3;
+				p += 3;
+			}
+		}
+
+		// Unlock pixel
+		image1->UnlockBits(ImageData1);
+		histImage->UnlockBits(histImageData);
+
+		// show it on the pictureBox
+		picture_box2->Image = histImage;
+	}
+	private: System::Void buttonThreshold_Click(System::Object^ sender, System::EventArgs^ e) {
+		// Threshold Filter 
+		
+		int threshold = 128;
+		
+		// declaration of BitMap object for results
+		Bitmap^ threshImage;
+		threshImage = gcnew Bitmap(image1->Width, image1->Height);
+
+		// declaration of BitMap object for pixels of results
+		Imaging::BitmapData^ threshImageData;
+
+		// Lock the image
+		threshImageData = threshImage->LockBits(rect, System::Drawing::Imaging::ImageLockMode::ReadWrite, image1->PixelFormat);
+
+		// thresh, set int ptr to the front of the image
+		threshResultPtr = threshImageData->Scan0;
+		Gr = (Byte*)((Void*)threshResultPtr);
+
+		// Lock the original image
+		ImageData1 = image1->LockBits(rect, System::Drawing::Imaging::ImageLockMode::ReadWrite, image1->PixelFormat);
+		// set int ptr to the front of the image
+		ptr = ImageData1->Scan0;
+		// ptr initialization
+		p = (Byte*)((Void*)ptr);
+
+		// process all pixel
+		for (int y = 0; y < image1->Height; y++)
+		{
+			for (int x = 0; x < image1->Width; x++)
+			{
+				// Threshold filter
+				if (p[0] > threshold || p[1] > threshold || p[2] > threshold)
+					pixel = 1;
+				else 
+					pixel = 0;
+				
+				Gr[0] = (Byte)pixel;
+				Gr[1] = (Byte)pixel;
+				Gr[2] = (Byte)pixel;
+
+				// Move to next pixel
+				Gr += 3;
+				p += 3;
+			}
+		}
+
+		// Unlock pixel
+		image1->UnlockBits(ImageData1);
+		threshImage->UnlockBits(threshImageData);
+
+		// show it on the pictureBox
+		picture_box2->Image = threshImage;
+	}
 	};
 }
